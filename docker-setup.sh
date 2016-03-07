fd6917f231df2406d01e9b521b324242fc7da434
Alin Voinea <alin@eaudeweb.ro>
2016-03-07 15:11:51 +0200

    Refs #69618 - Don't filter mail addresses

--- docker-setup.sh
+++ docker-setup.sh
@@ -10,6 +10,7 @@ function setup_conf_and_secret {
     postconf -e 'smtp_sasl_security_options = noanonymous'
     postconf -e 'smtp_tls_security_level = may'
     postconf -e 'mynetworks = 127.0.0.0/8 172.17.0.0/16'
+    postconf -e 'local_recipient_maps ='
 
     echo "$MTP_RELAY   $MTP_USER:$MTP_PASS" > /etc/postfix/relay_passwd
     postmap /etc/postfix/relay_passwd
